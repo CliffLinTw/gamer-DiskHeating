f6103b6783bb71c81e5135fb2f9b9a41f5843749
Chun-Yu Lin (cliff200905@gmail.com) <clifflin@eureka00.gpucluster.calab>
2020-10-30 11:36:47 +0800

    May 27

--- src/Init/Init_TestProb.cpp
+++ src/Init/Init_TestProb.cpp
@@ -31,7 +31,7 @@ void Init_TestProb_ELBDM_VortexPairLinear();
 void Init_TestProb_ELBDM_IsolatedHalo();
 void Init_TestProb_ELBDM_GaussianWavePacket();
 void Init_TestProb_ELBDM_LSS();
-
+void Init_TestProb_ELBDM_LSSHalo();
 
 
 
@@ -83,6 +83,7 @@ void Init_TestProb()
       case TESTPROB_ELBDM_ISOLATED_HALO :                Init_TestProb_ELBDM_IsolatedHalo();                break;
       case TESTPROB_ELBDM_GAUSSIAN_WAVE_PACKET :         Init_TestProb_ELBDM_GaussianWavePacket();          break;
       case TESTPROB_ELBDM_LSS :                          Init_TestProb_ELBDM_LSS();                         break;
+      case TESTPROB_ELBDM_LSS_HALO :                     Init_TestProb_ELBDM_LSSHalo();                     break;      
 
       default: Aux_Error( ERROR_INFO, "unsupported TESTPROB_ID (%d) !!\n", TESTPROB_ID );
    } // switch( TESTPROB_ID )
