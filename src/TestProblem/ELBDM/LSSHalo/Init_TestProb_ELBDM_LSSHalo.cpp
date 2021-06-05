fb2354dd152ab2cc00a1e49a18b81e85ddcc043a
Chun-Yu Lin (cliff200905@gmail.com) <clifflin@eureka00.gpucluster.calab>
2021-06-05 16:58:42 +0800

    Calculate CoM

--- src/TestProblem/ELBDM/LSSHalo/Init_TestProb_ELBDM_LSSHalo.cpp
+++ src/TestProblem/ELBDM/LSSHalo/Init_TestProb_ELBDM_LSSHalo.cpp
@@ -229,25 +229,6 @@ void SetGridIC( real fluid[], const double x, const double y, const double z, co
 ////
 //// Return      :  None
 ////-------------------------------------------------------------------------------------------------------
-
-
-
-
-//-------------------------------------------------------------------------------------------------------
-// Function    :  GetCenterOfMass_Disc_Heating
-// Description :  Record the center of mass (CM)
-//
-// Note        :  1. Invoked by Record_EridanusII() recursively
-//                2. Only include cells within CM_MaxR from CM_Old[] when updating CM
-//
-// Parameter   :  CM_Old[] : Previous CM
-//                CM_New[] : New CM to be returned
-//                CM_MaxR  : Maximum radius to compute CM
-//
-// Return      :  CM_New[]
-//-------------------------------------------------------------------------------------------------------
-
-/*
 void GetCenterOfMass_Disc_Heating( const double CM_Old[], double CM_New[], const double CM_MaxR )
 {
 
@@ -257,6 +238,7 @@ void GetCenterOfMass_Disc_Heating( const double CM_Old[], double CM_New[], const
    const bool   IntPhase_No       = false;
    const real   MinDens_No        = -1.0;
    const real   MinPres_No        = -1.0;
+   const real   MinTemp_No        = -1.0;
    const bool   DE_Consistency_No = false;
 #  ifdef PARTICLE
    const bool   TimingSendPar_No  = false;
@@ -299,7 +281,158 @@ void GetCenterOfMass_Disc_Heating( const double CM_Old[], double CM_New[], const
                          OPT__RHO_INT_SCHEME, INT_NONE, UNIT_PATCH, NSIDE_00, IntPhase_No, OPT__BC_FLU, BC_POT_NONE,
                          MinDens_No, MinPres_No, MinTemp_No, DE_Consistency_No );
 
-//  wrong    Prepare_PatchData( lv, Time[lv], TotalDens[0][0][0], 0, amr->NPatchComma[lv][1]/8, PID0List, _TOTAL_DENS,
+      delete [] PID0List;
+
+
+//    free memory for collecting particles from other ranks and levels, and free density arrays with ghost zones (rho_ext)
+#     ifdef PARTICLE
+      Par_CollectParticle2OneLevel_FreeMemory( lv, SibBufPatch, FaSibBufPatch );
+
+      Prepare_PatchData_FreeParticleDensityArray( lv );
+#     endif
+
+
+//    calculate the center of mass
+      const double dh = amr->dh[lv];
+      const double dv = CUBE( dh );
+
+      for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
+      {
+//       skip non-leaf patches
+         if ( amr->patch[0][lv][PID]->son != -1 )  continue;
+
+         const double x0 = amr->patch[0][lv][PID]->EdgeL[0] + 0.5*dh;
+         const double y0 = amr->patch[0][lv][PID]->EdgeL[1] + 0.5*dh;
+         const double z0 = amr->patch[0][lv][PID]->EdgeL[2] + 0.5*dh;
+
+         double x, y, z, dx, dy, dz;
+
+         for (int k=0; k<PS1; k++)  {  z = z0 + k*dh;  dz = z - CM_Old[2];
+                                       if ( Periodic ) {
+                                          if      ( dz > +HalfBox[2] )  {  z -= amr->BoxSize[2];  dz -= amr->BoxSize[2];  }
+                                          else if ( dz < -HalfBox[2] )  {  z += amr->BoxSize[2];  dz += amr->BoxSize[2];  }
+                                       }
+         for (int j=0; j<PS1; j++)  {  y = y0 + j*dh;  dy = y - CM_Old[1];
+                                       if ( Periodic ) {
+                                          if      ( dy > +HalfBox[1] )  {  y -= amr->BoxSize[1];  dy -= amr->BoxSize[1];  }
+                                          else if ( dy < -HalfBox[1] )  {  y += amr->BoxSize[1];  dy += amr->BoxSize[1];  }
+                                       }
+         for (int i=0; i<PS1; i++)  {  x = x0 + i*dh;  dx = x - CM_Old[0];
+                                       if ( Periodic ) {
+                                          if      ( dx > +HalfBox[0] )  {  x -= amr->BoxSize[0];  dx -= amr->BoxSize[0];  }
+                                          else if ( dx < -HalfBox[0] )  {  x += amr->BoxSize[0];  dx += amr->BoxSize[0];  }
+                                       }
+
+//          only include cells within CM_MaxR
+            const double R2 = SQR(dx) + SQR(dy) + SQR(dz);
+            if ( R2 < CM_MaxR2 )
+            {
+               const double dm = TotalDens[PID][k][j][i]*dv;
+
+               M_ThisRank     += dm;
+               MR_ThisRank[0] += dm*x;
+               MR_ThisRank[1] += dm*y;
+               MR_ThisRank[2] += dm*z;
+            }
+         }}}
+      } // for (int PID=0; PID<amr->NPatchComma[lv][1]; PID++)
+
+      delete [] TotalDens;
+   } // for (int lv=0; lv<NLEVEL; lv++)
+
+
+// collect data from all ranks to calculate the CM
+// --> note that all ranks will get CM_New[]
+   MPI_Allreduce( &M_ThisRank, &M_AllRank, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+   MPI_Allreduce( MR_ThisRank, MR_AllRank, 3, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+
+   for (int d=0; d<3; d++)    CM_New[d] = MR_AllRank[d] / M_AllRank;
+
+// map the new CM back to the simulation domain
+   if ( Periodic )
+   for (int d=0; d<3; d++)
+   {
+      if      ( CM_New[d] >= amr->BoxSize[d] )  CM_New[d] -= amr->BoxSize[d];
+      else if ( CM_New[d] < 0.0              )  CM_New[d] += amr->BoxSize[d];
+
+   }
+
+   for (int d=0; d<3; d++)
+      if ( CM_New[d] >= amr->BoxSize[d]  ||  CM_New[d] < 0.0 )
+         Aux_Error( ERROR_INFO, "CM_New[%d] = %14.7e lies outside the domain !!\n", d, CM_New[d] );
+
+} // FUNCTION : GetCenterOfMass_Disc_Heating
+
+/*
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  GetCenterOfMass_Disc_Heating
+// Description :  Record the center of mass (CM)
+//
+// Note        :  1. Invoked by Record_EridanusII() recursively
+//                2. Only include cells within CM_MaxR from CM_Old[] when updating CM
+//
+// Parameter   :  CM_Old[] : Previous CM
+//                CM_New[] : New CM to be returned
+//                CM_MaxR  : Maximum radius to compute CM
+//
+// Return      :  CM_New[]
+//-------------------------------------------------------------------------------------------------------
+
+
+void GetCenterOfMass_Disc_Heating( const double CM_Old[], double CM_New[], const double CM_MaxR )
+{
+
+   const double CM_MaxR2          = SQR( CM_MaxR );
+   const double HalfBox[3]        = { 0.5*amr->BoxSize[0], 0.5*amr->BoxSize[1], 0.5*amr->BoxSize[2] };
+   const bool   Periodic          = ( OPT__BC_FLU[0] == BC_FLU_PERIODIC );
+   const bool   IntPhase_No       = false;
+   const real   MinDens_No        = -1.0;
+   const real   MinPres_No        = -1.0;
+   const real   MinTemp_No        = -1.0;
+   const bool   DE_Consistency_No = false;
+#  ifdef PARTICLE
+   const bool   TimingSendPar_No  = false;
+   const bool   PredictParPos_No  = false;
+   const bool   JustCountNPar_No  = false;
+#  ifdef LOAD_BALANCE
+   const bool   SibBufPatch       = true;
+   const bool   FaSibBufPatch     = true;
+#  else
+   const bool   SibBufPatch       = NULL_BOOL;
+   const bool   FaSibBufPatch     = NULL_BOOL;
+#  endif
+#  endif // #ifdef PARTICLE
+
+   int   *PID0List = NULL;
+   double M_ThisRank, MR_ThisRank[3], M_AllRank, MR_AllRank[3];
+   real (*TotalDens)[PS1][PS1][PS1];
+
+   M_ThisRank = 0.0;
+   for (int d=0; d<3; d++)    MR_ThisRank[d] = 0.0;
+
+
+   for (int lv=0; lv<NLEVEL; lv++)
+   {
+//    initialize the particle density array (rho_ext) and collect particles to the target level
+#     ifdef PARTICLE
+      Prepare_PatchData_InitParticleDensityArray( lv );
+
+      Par_CollectParticle2OneLevel( lv, PredictParPos_No, NULL_REAL, SibBufPatch, FaSibBufPatch, JustCountNPar_No,
+                                    TimingSendPar_No );
+#     endif
+
+//    get the total density on grids
+      TotalDens = new real [ amr->NPatchComma[lv][1] ][PS1][PS1][PS1];
+      PID0List  = new int  [ amr->NPatchComma[lv][1]/8 ];
+
+      for (int PID0=0, t=0; PID0<amr->NPatchComma[lv][1]; PID0+=8, t++)    PID0List[t] = PID0;
+
+//      Prepare_PatchData( lv, Time[lv], TotalDens[0][0][0], NULL, 0, amr->NPatchComma[lv][1]/8, PID0List, _TOTAL_DENS, _NONE,
+//                         OPT__RHO_INT_SCHEME, INT_NONE, UNIT_PATCH, NSIDE_00, IntPhase_No, OPT__BC_FLU, BC_POT_NONE,
+//                         MinDens_No, MinPres_No, MinTemp_No, DE_Consistency_No );
+
+      Prepare_PatchData( lv, Time[lv], TotalDens[0][0][0], 0, amr->NPatchComma[lv][1]/8, PID0List, _TOTAL_DENS,
                          OPT__RHO_INT_SCHEME, UNIT_PATCH, NSIDE_00, IntPhase_No, OPT__BC_FLU, BC_POT_NONE,
                          MinDens_No, MinPres_No, DE_Consistency_No );
 
@@ -384,6 +517,7 @@ void GetCenterOfMass_Disc_Heating( const double CM_Old[], double CM_New[], const
          Aux_Error( ERROR_INFO, "CM_New[%d] = %14.7e lies outside the domain !!\n", d, CM_New[d] );
 
 } // FUNCTION : GetCenterOfMass_Disc_Heating
+
 
 */
 
@@ -711,6 +845,47 @@ void Record_Disc_Heating()
                               recv[min_pote_rank][6], recv[min_pote_rank][7], recv[min_pote_rank][8], recv[min_pote_rank][9] );
       fclose( file_center );
    } // if ( MPI_Rank == 0 )
+
+   // compute the center of mass until convergence
+      const double TolErrR2 = SQR( Soliton_CM_TolErrR );
+      const int    NIterMax = 10;
+
+      double dR2, CM_Old[3], CM_New[3];
+      int NIter = 0;
+
+   // set an initial guess by the peak density position
+      if ( MPI_Rank == 0 )
+         for (int d=0; d<3; d++)    CM_Old[d] = recv[max_dens_rank][3+d];
+
+      MPI_Bcast( CM_Old, 3, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+
+      while ( true )
+      {
+         GetCenterOfMass_Disc_Heating( CM_Old, CM_New, Soliton_CM_MaxR );
+
+         dR2 = SQR( CM_Old[0] - CM_New[0] )
+             + SQR( CM_Old[1] - CM_New[1] )
+             + SQR( CM_Old[2] - CM_New[2] );
+         NIter ++;
+
+         if ( dR2 <= TolErrR2  ||  NIter >= NIterMax )
+            break;
+         else
+            memcpy( CM_Old, CM_New, sizeof(double)*3 );
+      }
+
+      if ( MPI_Rank == 0 )
+      {
+         if ( dR2 > TolErrR2 )
+            Aux_Message( stderr, "WARNING : dR (%13.7e) > Soliton_CM_TolErrR (%13.7e) !!\n", sqrt(dR2), \ );
+
+         FILE *file_center = fopen( filename_center, "a" );
+         fprintf( file_center, "  %10d  %14.7e  %14.7e  %14.7e\n", NIter, CM_New[0], CM_New[1], CM_New[2] );
+         fclose( file_center );
+      }
+
+
+      for (int d=0; d<3; d++)    Tidal_CM[d] = CM_New[d];
 
    delete [] recv;
 
